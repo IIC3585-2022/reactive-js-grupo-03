8daa54c04ced9777600e1550abc1726d10e597a1
agustin-rios <arios6@uc.cl>
2022-03-30 14:55:07 -0300

    feat(./): load html window

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+var http = require('http'),
+    fs = require('fs');
+
+http.createServer(function (_, response) {
+    fs.readFile('./src/views/layout.html', function (err, data) {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(data);
+        response.end();
+    });
+}).listen(8000);
+
+// Source: https://stackoverflow.com/a/4728481
