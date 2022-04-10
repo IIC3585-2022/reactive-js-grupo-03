cb0cfbf66c0c0bbbc3631150e8a190937a9a1703
agustin-rios <arios6@uc.cl>
2022-04-10 19:33:13 -0400

    feat(mobs): draw mobs in map

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ const gameDrawable = drawGame(gameGrid);
 gameDrawable.setMap(MAP_PICTURE.map);
 gameDrawable.drawMaze();
 gameDrawable.drawPoints();
+gameDrawable.drawMob([{
+    x: 12,
+    y: 17,
+    number: 0,
+    image: 'https://upload.wikimedia.org/wikipedia/commons/2/26/Pacman_HD.png',
+}, {
+    x: 14,
+    y: 17,
+    number: 0,
+    image: 'https://upload.wikimedia.org/wikipedia/commons/2/26/Pacman_HD.png',
+}]);
